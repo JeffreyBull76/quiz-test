1202f9a98eb7800cc44817fd1614e07a8149a182
JeffreyBull76 <jeffreybull@sky.com>
2022-10-28 00:52:01 +0000

    reinstate full code to work with list

--- run.py
+++ run.py
@@ -13,82 +13,75 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('python_quiz_questions')
 
-
 print("WELCOME TO THE QUIZ - ANSWER EACH QUESTION TO PROCEED !\n")
 print("Your answers are given by selecting the letter for each choice")
 print("So you if you think the answer is B you would type B")
 
 
-# def validate_question(answer):
-#     """
-#     Checks for valid answer
-#     """
-#     if answer.upper() not in ("A", "B", "C"):
-#         print("Answer is not valid!")
-#         print("Please answer with A, B or C")
-#         return False
-#     else:
-#         print("Answer is valid!")
-#         return True
+QLIST = []
 
 
-# def check_question(answer, active_quest):
-#     """
-#     Checks to see if answer is correct or incorrect
-#     """
-#     quest = SHEET.worksheet('questions')
-#     quest_num = active_quest
-
-#     if answer.upper() == quest.col_values(5)[quest_num]:
-#         print("correct")
-#     else:
-#         print("incorrect")
-
-
-# def ask_question():
-#     """
-#     Question function
-#     """
-#     quest = SHEET.worksheet('questions')
-#     quest_num = 1
-
-#     while quest_num <= 5:
-#         print(quest.col_values(1)[quest_num])
-#         for i in range(2, 5):
-#             print(
-#                 f"{quest.col_values(i)[0]}: {quest.col_values(i)[quest_num]}"
-#                 )
-
-#         answer = input("Enter your answer here:\n")
-
-#         if validate_question(answer):
-#             check_question(answer, quest_num)
-#             quest_num += 1
-
-
-que_cat = ['Question', 'Option A', 'Option B', 'Option C', 'Answer']
-
-
-def set_question():
+def set_questions():
     """
     Question function
     """
     quest = SHEET.worksheet('questions')
     quest_num = 1
+    print("Please wait building question database...\n")
 
     while quest_num <= 5:
         for i in range(1, 6):
-            print(
-                f"{quest.col_values(i)[0]}: {quest.col_values(i)[quest_num]}"
+            QLIST.extend(
+                [quest.col_values(i)[0], quest.col_values(i)[quest_num]]
                 )
 
         quest_num += 1
-            
-
-set_question()
 
 
-# ask_question()
+set_questions()
+
+
+def validate_question(answer):
+    """
+    Checks for valid answer
+    """
+    if answer.upper() not in ("A", "B", "C"):
+        print("Answer is not valid!")
+        print("Please answer with A, B or C")
+        return False
+    else:
+        print("Answer is valid!")
+        return True
+
+
+def check_question(answer):
+    """
+    Checks to see if answer is correct or incorrect
+    """
+    if answer.upper() == QLIST[9]:
+        print("correct")
+    else:
+        print("incorrect")
+
+
+def ask_question(rang1, rang2):
+    """
+    Question function
+    """
+    quest_cnt = 1
+
+    while quest_cnt < 2:
+        for i in range(rang1, rang2, 2):
+            print(f"{QLIST[i]}: {QLIST[i+1]}")
+        
+        answer = input("Enter your answer here:\n")
+
+        if validate_question(answer):
+            check_question(answer)
+            quest_cnt += 1
+
+
+ask_question(0, 10)
 
 # loop now correctly checks for a valid answer 
 # NEXT we must track and itterate the players score
