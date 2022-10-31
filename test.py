f4cce0d4b5e9320ffccd08dbfae3d5bb53999089
JeffreyBull76 <jeffreybull@sky.com>
2022-10-31 02:45:14 +0000

    define more efficient function for grabbing questions

---
+++ test.py
@@ -0,0 +1,21 @@
+import gspread
+import sys
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('python_quiz_questions')
+
+
+list_of_lists = SHEET.worksheet('1').get_all_values()
+
+
+print(list_of_lists[8][0])
